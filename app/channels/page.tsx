f75345fd248775ec58e88046923727807bcd6f0d
littlecm <clittle.norml@gmail.com>
2024-10-17 22:57:33 -0400

    Update page.tsx

--- app/channels/page.tsx
+++ app/channels/page.tsx
@@ -6,7 +6,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { PlusCircle, Trash2 } from 'lucide-react';
-import { useGroups } from './groups';
+import { useGroups } from '../groups/page';
 
 interface Field {
   name: string;
